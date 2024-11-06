f81474999882d8c9ca20e621d23325b37013c67b
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2024-11-06 19:59:40 +0700

    create : searchview pake binding
    masih broken

--- app/src/main/java/paba/b/latihanlistview/MainActivity.kt
+++ app/src/main/java/paba/b/latihanlistview/MainActivity.kt
@@ -4,30 +4,38 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import paba.b.latihanlistview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var data = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        var data = mutableListOf<String>()
         data.addAll(listOf("1","2","3","4","5"))
 
         val lvAdapter = ArrayAdapter(
             this, android.R.layout.simple_list_item_1,
             data
         )
+
+        binding.lv1.adapter = lvAdapter
 
         val _lv1 = findViewById<ListView>(R.id.lv1)
         _lv1.adapter = lvAdapter
@@ -52,5 +60,16 @@ class MainActivity : AppCompatActivity() {
             lvAdapter.notifyDataSetChanged()
         }
 
+        binding.sv1.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+          override fun onQueryTextSubmit(query: String?): Boolean {
+              lvAdapter.getFilter().filter(query)
+              return false
+          }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                lvAdapter.getFilter().filter(newText)
+                return false
+            }
+        })
     }
 }
